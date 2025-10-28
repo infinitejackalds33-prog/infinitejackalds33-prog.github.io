f26c3a6ef86649ebf0e9adcdc4576cae2025a6cc
Infinite Jackal <infinitejackalds33@gmail.com>
2025-10-28 20:08:23 +0300

    Some Fixed

--- manifest.js
+++ manifest.js
@@ -1,7 +1,7 @@
 {
   "name": "Murko Links",
   "short_name": "Murko",
-  "description": "Все официальные ссылки Мурко в одном месте",
+  "description": "Все официальные ссылки Мурко",
   "start_url": "/",
   "display": "standalone",
   "background_color": "#050505",
@@ -13,29 +13,13 @@
     {
       "src": "icon-192.png",
       "sizes": "192x192",
-      "type": "image/png",
-      "purpose": "any maskable"
+      "type": "image/png"
     },
     {
-      "src": "icon-512.png",
+      "src": "icon-512.png", 
       "sizes": "512x512",
-      "type": "image/png",
-      "purpose": "any maskable"
+      "type": "image/png"
     }
   ],
-  "categories": ["entertainment", "social"],
-  "screenshots": [
-    {
-      "src": "screenshot-wide.png",
-      "sizes": "1280x720",
-      "type": "image/png",
-      "form_factor": "wide"
-    },
-    {
-      "src": "screenshot-narrow.png",
-      "sizes": "375x812",
-      "type": "image/png",
-      "form_factor": "narrow"
-    }
-  ]
+  "categories": ["entertainment", "social"]
 }
